2e14ef8eb3f12b31d4876cdc511d1265cd069010
xarop-pa-toss <rpgdsantos@gmail.com>
2024-06-06 23:03:43 +0100

    Created MapHandler. Not working on localhost test

--- 1-QuizGame/go.mod
+++ 1-QuizGame/go.mod
@@ -1,3 +1,3 @@
-module Gophercises
+module 1-QuizGame
 
 go 1.22.3
--- 2-URLShortener/urlshort/handler.go
+++ 2-URLShortener/urlshort/handler.go
@@ -11,8 +11,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -23,8 +29,8 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // YAML is expected to be in the format:
 //
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
 //
 // The only errors that can be returned all related to having
 // invalid YAML data.
