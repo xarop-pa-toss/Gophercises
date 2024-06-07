757d66526e97539d22ff3f3af00eb785848a5a12
xarop-pa-toss <rpgdsantos@gmail.com>
2024-06-07 22:54:28 +0100

    Included MapHandler

--- 2-URLShortener/main/main.go
+++ 2-URLShortener/main/main.go
@@ -30,7 +30,7 @@ func main() {
 	// 		panic(err)
 	// 	}
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":808", mapHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
